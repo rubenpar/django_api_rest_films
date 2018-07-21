c985620f69d383ff00999878f8b2ca5d7cc78dc1
Unknown <rubenparnu@gmail.com>
2018-07-21 22:13:55 +0200

    refactor code

--- filmsapi/api/urls.py
+++ filmsapi/api/urls.py
@@ -1,7 +1,7 @@
 
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import *
+from .views import PeopleCreateView, StudioCreateView, FilmCreateView
 
 urlpatterns = {
     url(r'^people$', PeopleCreateView.as_view(), name="create"),
