33e7b084bc04c9d10a8fabd7b58cd3573e389ef3
Unknown <rubenparnu@gmail.com>
2018-07-22 13:40:28 +0200

    refactor models tests

--- filmsapi/api/tests.py
+++ filmsapi/api/tests.py
@@ -3,9 +3,13 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 
-from .models import People
+from .models import People, Studio, Film
 
 from datetime import datetime
+
+#################
+## Test Models ##
+#################
 
 class PeopleTestCase(TestCase):
     """
@@ -16,11 +20,11 @@ class PeopleTestCase(TestCase):
         """
         Defines the test variables
         """
-        self.name = "Christopher Nolan"
+        self.name = "test_a"
         self.birth_date = "02/02/1975"
         self.actor = People(name=self.name, birth_date=self.birth_date)
 
-    def test_model_can_create_a_bucketlist(self):
+    def test_model_can_create_a_person(self):
         """
         Test the People model can create a person.
         """
@@ -29,70 +33,93 @@ class PeopleTestCase(TestCase):
         new_count = People.objects.count()
         self.assertNotEqual(old_count, new_count)
 
-
-
-class PeopleViewTestCase(TestCase):
+class StudioTestCase(TestCase):
     """
-    Test suite for the api views.
+    Defines the Studio model test case
     """
 
     def setUp(self):
         """
-        Define the test client and other test variables.
+        Defines the test variables
         """
+        self.name = "test_a"
+        self.city = "test_city"
+        self.studio = Studio(name=self.name, city=self.city)
 
-        self.client = APIClient()
-        self.actor_data = {'name': 'Christopher Nolan', 'birth_date': '02/02/1975'}
-        self.response = self.client.post(
-            reverse('create'),
-            self.actor_data,
-            format="json")
-
-    def test_api_can_create_a_person(self):
+    def test_model_can_create_a_studio(self):
         """
-        Test the api has person creation capability.
+        Test the Studio model can create a studio.
         """
+        old_count = Studio.objects.count()
+        self.studio.save()
+        new_count = Studio.objects.count()
+        self.assertNotEqual(old_count, new_count)
 
-        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+class FilmTestCase(TestCase):
+    """
+    Defines the Film model test case
+    """
 
-    def test_api_can_get_people(self):
+    def setUp(self):
         """
-        Test the api can get the people list.
+        Defines the test variables
         """
+        self.title = "test_a"
+        self.studio = Studio.objects.create(name="test_b", city="test_b")
+        self.release_date = "1st Feb 2015"
+        self.director = People.objects.create(name="test_b", birth_date="00/00/2000")
+        self.actors = [People.objects.create(name="test_", birth_date="00/00/2000")]
+        self.film = Film.objects.create(title=self.title, 
+                            studio=self.studio,
+                            release_date=self.release_date,
+                            director = self.director,
+                            )
 
-        people = People.objects.get()
-        response = self.client.get(
-            reverse('details',
-            kwargs={'pk': people.id}),
-            format='json'
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertContains(response, people)
-
-    def test_api_can_update_people(self):
+    def test_model_can_create_a_film(self):
         """
-        Test the api can update a given person.
+        Test the Film model can create a film.
         """
-        change_person = {'name': 'Something new'}
-        people = People.objects.get()
-        res = self.client.put(
-            reverse('details', 
-            kwargs={'pk': people.id}),
-            change_person, 
-            format='json'
-        )
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.film.title, "test_a")
 
-    def test_api_can_delete_people(self):
-        """
-        Test the api can delete a person.
-        """
-        people = People.objects.get()
-        response = self.client.delete(
-            reverse('details', 
-            kwargs={'pk': people.id}),
-            format='json',
-            follow=True)
+#################
+## Test Api ##
+#################
+# class PeopleViewTestCase(TestCase):
+#     """
+#     Test suite for the api views.
+#     """
 
-        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+#     def setUp(self):
+#         """
+#         Define the test client and other test variables.
+#         """
 
+#         self.client = APIClient()
+#         self.actor_data = {'name': 'Christopher Nolan', 'birth_date': '02/02/1975'}
+#         self.response = self.client.post(
+#             reverse('create'),
+#             self.actor_data,
+#             format="json")
+
+#     def test_api_can_create_a_person(self):
+#         """
+#         Test the api has person creation capability.
+#         """
+
+    #     self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    # def test_api_can_get_people(self):
+    #     """
+    #     Test the api can get the people list.
+    #     """
+
+    #     people = People.objects.get()
+    #     response = self.client.get(
+    #         reverse('details',
+    #         kwargs={'pk': people.id}),
+    #         format='json'
+    #     )
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertContains(response, people)
+
+    
